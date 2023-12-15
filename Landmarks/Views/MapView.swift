9ce113f2b96c7516a121f12cce6dcf13320ffe2c
quatarnary <contact@ilyasbugra.com>
2023-12-15 06:29:55 +0300

    section 5 completed

---
+++ Landmarks/Views/MapView.swift
@@ -0,0 +1,28 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Bugra Aslan on 15.12.2023.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var body: some View {
+        Map(initialPosition: .region(region))
+    }
+    
+    private var region: MKCoordinateRegion {
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 34.011_286,
+                                           longitude: -116.166_868),
+            span: MKCoordinateSpan(latitudeDelta: 0.2,
+                                   longitudeDelta: 0.2)
+            )
+    }
+}
+
+#Preview {
+    MapView()
+}
