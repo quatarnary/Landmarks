5497f2b2178638b00d717d5aa0739606b19b37b0
quatarnary <contact@ilyasbugra.com>
2023-12-15 07:03:24 +0300

    tutorial break step-6, section-1: building-lists-and-navigation

---
+++ Landmarks/Models/Landmark.swift
@@ -0,0 +1,23 @@
+    //
+    //  Landmark.swift
+    //  Landmarks
+    //
+    //  Created by Bugra Aslan on 15.12.2023.
+    //
+
+import Foundation
+import SwiftUI
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+}
