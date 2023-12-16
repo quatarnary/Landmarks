a887c06bc0f6861685925cc6f94a40be76047ebe
quatarnary <contact@ilyasbugra.com>
2023-12-16 23:53:29 +0300

    section-3 completed

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -1,9 +1,9 @@
-//
-//  LandmarkList.swift
-//  Landmarks
-//
-//  Created by Bugra Aslan on 16.12.2023.
-//
+    //
+    //  LandmarkList.swift
+    //  Landmarks
+    //
+    //  Created by Bugra Aslan on 16.12.2023.
+    //
 
 import SwiftUI
 
@@ -18,13 +18,20 @@ struct LandmarkList: View {
     
     var body: some View {
         NavigationSplitView {
-            List(filteredLandmarks) { landmark in
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites Only")
+                }
+                
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
                 }
             }
+            .animation(.default, value: filteredLandmarks)
             .navigationTitle("Landmarks")
         } detail: {
             ZStack {
