9822143466c52152c1578b85bc887a7cda0a0c28
Ilyas Aslan <152633183+quatarnary@users.noreply.github.com>
2023-12-18 19:29:15 +0300

    section-1 completed from windows-github/code

--- Landmarks/Views/Categories/CategoryHome.swift
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -1,5 +1,24 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Bugra Aslan on 18.12.2023.
+//
+
 import SwiftUI
 
 struct CategoryHome: View {
-    
+    var body: some View {
+        NavigationSplitView {
+            Test("Hello, World!")
+                .navigationTitle("Featured")
+        } detail {
+            Text("Select a Landmark")
+        }
+    }
+}
+
+
+#Preview {
+    CategoryHome()
 }
