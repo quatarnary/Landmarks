45ed80484e2ff5ae226ebda0a87aa43e2db308d4
quatarnary <contact@ilyasbugra.com>
2023-12-19 05:17:09 +0300

    apperantly xcode didn't liked something about the file created using github.dev. So I deleted the file and added again, it seems like preview works again

--- Landmarks/Views/Categories/CategoryHome.swift
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -2,7 +2,7 @@
 //  CategoryHome.swift
 //  Landmarks
 //
-//  Created by Bugra Aslan on 18.12.2023.
+//  Created by Bugra Aslan on 19.12.2023.
 //
 
 import SwiftUI
@@ -10,14 +10,13 @@ import SwiftUI
 struct CategoryHome: View {
     var body: some View {
         NavigationSplitView {
-            Text("Hello, World!")
+            Text("Hello, World")
                 .navigationTitle("Featured")
         } detail: {
             Text("Select a Landmark")
         }
     }
 }
-
 
 #Preview {
     CategoryHome()
