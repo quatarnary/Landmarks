3d3e3de6b52beaf9b54135b8843c70a02325372b
quatarnary <contact@ilyasbugra.com>
2023-12-19 12:28:59 +0300

    section-1 completed

---
+++ Landmarks/Views/PageView/Helpers/PageViewController.swift
@@ -0,0 +1,28 @@
+//
+//  PageViewController.swift
+//  Landmarks
+//
+//  Created by Bugra Aslan on 19.12.2023.
+//
+
+import SwiftUI
+import UIKit
+
+struct PageViewController<Page: View>: UIViewControllerRepresentable {
+    var pages: [Page]
+    
+    func makeUIViewController(context: Context) -> UIPageViewController {
+        let pageViewController = UIPageViewController(
+            transitionStyle: .scroll,
+            navigationOrientation: .horizontal
+        )
+        
+        return pageViewController
+    }
+    
+    func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
+        pageViewController.setViewControllers(
+            [UIHostingController(rootView: pages[0])], direction: .forward, animated: true
+        )
+    }
+}
---
+++ Landmarks/Views/PageView/PageView.swift
@@ -0,0 +1,21 @@
+//
+//  PageView.swift
+//  Landmarks
+//
+//  Created by Bugra Aslan on 19.12.2023.
+//
+
+import SwiftUI
+
+struct PageView<Page: View>: View {
+    var pages: [Page]
+    
+    var body: some View {
+        PageViewController(pages: pages)
+            .aspectRatio(3 / 2, contentMode: .fit)
+    }
+}
+
+#Preview {
+    PageView(pages: ModelData().features.map { FeatureCard(landmark: $0) })
+}
