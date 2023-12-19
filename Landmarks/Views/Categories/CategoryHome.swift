93ee258a9ed15e0bdccb0c0adf9e55af3d859fc1
quatarnary <contact@ilyasbugra.com>
2023-12-19 05:10:19 +0300

    adjusting windows changes for xcode: minor fixes and adding files to project

--- Landmarks/Views/Categories/CategoryHome.swift
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -10,9 +10,9 @@ import SwiftUI
 struct CategoryHome: View {
     var body: some View {
         NavigationSplitView {
-            Test("Hello, World!")
+            Text("Hello, World!")
                 .navigationTitle("Featured")
-        } detail {
+        } detail: {
             Text("Select a Landmark")
         }
     }
