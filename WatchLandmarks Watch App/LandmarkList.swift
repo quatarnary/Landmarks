cc7e946bb7f4743c54aa07975863adf860509d9c
quatarnary <contact@ilyasbugra.com>
2023-12-23 19:17:00 +0300

    section-3 completed

--- WatchLandmarks Watch App/LandmarkList.swift
+++ WatchLandmarks Watch App/LandmarkList.swift
@@ -1,9 +1,9 @@
-    //
-    //  LandmarkList.swift
-    //  Landmarks
-    //
-    //  Created by Bugra Aslan on 16.12.2023.
-    //
+//
+//  LandmarkList.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Bugra Aslan on 23.12.2023.
+//
 
 import SwiftUI
 
@@ -34,28 +34,8 @@ struct LandmarkList: View {
             }
             .animation(.default, value: filteredLandmarks)
             .navigationTitle("Landmarks")
-            .frame(minWidth: 300)
         } detail: {
-            ZStack {
-                Image("ink-background-texture")
-                    .resizable()
-                    .scaledToFill()
-                    .blur(radius: 10.0)
-                    .edgesIgnoringSafeArea(.all)
-                
-                Color.black.opacity(0.6)
-                    .edgesIgnoringSafeArea(.all)
-                
-                Text("Select a Landmark")
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
-                    .foregroundColor(.white)
-                    .shadow(color: .black.opacity(0.8), radius: 5, x: 0, y: 2)
-                    .padding()
-                    .background(Color.black.opacity(0.5).cornerRadius(10))
-                    .padding()
-            }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            Text("Select a Landmark")
         }
     }
 }
