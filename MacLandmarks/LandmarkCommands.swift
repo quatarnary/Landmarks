d7904008f61f7628abf2bdde78f95f8b4a089806
quatarnary <contact@ilyasbugra.com>
2024-01-01 20:24:26 +0300

    section 5 completed

---
+++ MacLandmarks/LandmarkCommands.swift
@@ -0,0 +1,15 @@
+//
+//  LandmarkCommands.swift
+//  Landmarks
+//
+//  Created by Bugra Aslan on 1.01.2024.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        SidebarCommands()
+    }
+    
+}
